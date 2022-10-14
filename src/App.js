db46f16b069e2bfdc83a2d9620ad762402dbe0d5
Poornima <10817230+poorninaik@users.noreply.github.com>
2022-10-14 19:50:43 +0000

    upgraded to react 18

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import { useContext } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Intro from "./components/Intro";
 import ProductList from "./components/ProductList";
 import Toggle from "./components/Toggle";
-import { ThemeContext } from "./Context";
 
 const App = () => {
-  const theme = useContext(ThemeContext);
-  const darkMode = theme.state.darkMode;
   return (
-    <div
-      style={{
-        backgroundColor: darkMode ? "#222" : "white",
-        color: darkMode && "white",
-      }}
-    >
+    <div>
       <Toggle />
       <Intro />
       <About />
--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import App from "./App";
 import { ThemeProvider } from "./Context";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
