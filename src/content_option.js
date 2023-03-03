6058ee1887eb1ff81fb3cd29485500e0b4fc886d
Poornima Naik <naikintech@gmail.com>
2023-03-03 14:35:13 -0800

    updated code to latest reactjs

---
+++ src/content_option.js
@@ -0,0 +1,89 @@
+const logotext = "Naik";
+const meta = {
+    title: "Poornima Naik",
+    description: "I’m Poornima Naik Full stack devloper,currently working in San Jose, CA ",
+};
+
+const introdata = {
+    title: "I’m Poornima Naik",
+    animated: {
+        first: "I enjoy coding",
+        second: "Follow me to know more",
+    },
+    description: "",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "Engineer with 14+ years experience in developing scalable distributed full stack applications using cutting-edge technologies.",
+};
+const worktimeline = [{
+        jobtitle: "Full Stack Engineer",
+        where: <img src="https://www.audiblemagic.com/wp-content/uploads/2019/08/favicon.png" width="60px" alt="logo"></img>,
+        date: "10/2019 to 03/2013",
+    }
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "",
+        link: "#",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "naikintech@gmail.com",
+    YOUR_FONE: "",
+    description: "",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/poorninaik",
+    linkedin: "https://www.linkedin.com/in/poornimanaik/"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
